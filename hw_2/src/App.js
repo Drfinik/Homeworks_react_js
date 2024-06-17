8376ca25e9b0799ea2ae67e90f67894c1370d2dd
Drfinik <freelanceap@yahoo.com>
2024-06-17 09:29:29 +0300

    save hw_2

---
+++ hw_2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CommentsList from './components/CommentsList';
+ 
+const App = () => {
+  return (
+    <div>
+      <h1>Комментарии</h1>
+      <CommentsList />
+    </div>
+  );
+};
+ 
+export default App;
