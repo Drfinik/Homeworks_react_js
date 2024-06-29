ce11a5e0dd1c9cfc0adab0da001a02157a4c3e9c
Drfinik <freelanceap@yahoo.com>
2024-06-29 09:47:14 +0300

    save hw_5

---
+++ hw_5/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import ThemeSwitcher from './components/ThemeSwitcher';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div>        
+        <ThemeSwitcher />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ hw_5/src/actions.js
@@ -0,0 +1,5 @@
+// Action types
+const TOGGLE_THEME = 'TOGGLE_THEME';
+
+// Action creator
+export const toggleTheme = () => ({ type: TOGGLE_THEME });
